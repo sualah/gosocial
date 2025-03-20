183b952d0d7ae101245cfa5fdb372d17032f8c5f
sualah <sualahabdullai@gmail.com>
2025-03-20 11:36:51 +0000

    chore(app): created gosocial

---
+++ cmd/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"github.com/sualah/gosocial/internal/env"
+)
+
+func main() {
+	cfg := config{
+		addr: env.GetString("ADDR",  ":8080"),
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	mux := app.mount()
+
+	log.Fatal(app.run(mux))
+}
